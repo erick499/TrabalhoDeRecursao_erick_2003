4467ccb954536f8764b8feb1e746503ce5a9ab63
erick499 <erick.t.oliveira@gmail.com>
2015-03-06 20:48:24 -0300

    trabalho do lolo
    trabalho pedido pelo professor antoanne pontes no dia 06/03/15.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recursiva = new System.Windows.Forms.Button();
+            this.iterativa = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tota1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // recursiva
+            // 
+            this.recursiva.Location = new System.Drawing.Point(12, 104);
+            this.recursiva.Name = "recursiva";
+            this.recursiva.Size = new System.Drawing.Size(75, 23);
+            this.recursiva.TabIndex = 0;
+            this.recursiva.Text = "recursiva";
+            this.recursiva.UseVisualStyleBackColor = true;
+            this.recursiva.Click += new System.EventHandler(this.recur);
+            // 
+            // iterativa
+            // 
+            this.iterativa.Location = new System.Drawing.Point(197, 104);
+            this.iterativa.Name = "iterativa";
+            this.iterativa.Size = new System.Drawing.Size(75, 23);
+            this.iterativa.TabIndex = 1;
+            this.iterativa.Text = "iterativa";
+            this.iterativa.UseVisualStyleBackColor = true;
+            this.iterativa.Click += new System.EventHandler(this.iter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // tota1
+            // 
+            this.tota1.AutoSize = true;
+            this.tota1.Location = new System.Drawing.Point(153, 75);
+            this.tota1.Name = "tota1";
+            this.tota1.Size = new System.Drawing.Size(0, 13);
+            this.tota1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Resultado: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tota1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.iterativa);
+            this.Controls.Add(this.recursiva);
+            this.Name = "Form1";
+            this.Text = "Trab_ERICK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button recursiva;
+        private System.Windows.Forms.Button iterativa;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label tota1;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double a;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void recur(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+
+            tota1.Text = resultado_recur(a).ToString();
+        }
+
+        private void iter(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+
+            tota1.Text = resultado_iter(a).ToString();
+        }
+
+        private double resultado_iter(double n) {
+
+            double r = n;
+
+            for (double i = n - 1; i > 0; i--)
+            {
+                r = r * (i);
+            }
+
+            return r;
+        
+        }
+
+        private double resultado_recur(double n)
+        {
+
+            if (n <= 1) return n = 1;
+
+            return n * resultado_recur(n-1) ;
+        }
+    }
+}
